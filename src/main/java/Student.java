fa2b62af9f49bda247a7592d7d59f8dfb77f5a6c
Ibrahim Branham <ibrahimbranham@Ibrahims-iMac.local>
2018-06-28 14:20:46 -0400

    student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -24,4 +24,53 @@
 
 public class Student {
 
+    private String firstname;
+    private String lastname;
+    private static int studentID;
+    private  String email;
+    private String password;
+
+    public Student(String newFirstName,String newLastName,String newEmail,String newPassword ){
+
+        this.firstname = newFirstName;
+        this.lastname = newLastName;
+        this.studentID +=1;
+        this.email = newEmail;
+        this.password = newPassword;
+
+
+
+    }
+
+    public String getFirstName(){
+        return firstname;
+    }
+
+    public void setFirstname(String newfirstname ){
+        this.firstname = newfirstname;
+    }
+
+    public String getLastName(){
+        return lastname;
+    }
+
+    public void setLastname(String newlastname ){
+        this.lastname = newlastname;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String newEmail ){
+        this.email = newEmail;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String newPassword ){
+        this.password = newPassword;
+    }
 }
