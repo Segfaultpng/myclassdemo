db97793ebe142378d2a37aefb157ff01299ccc49
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-06-28 14:36:42 -0400

    test

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,7 +1,24 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StudentTest {
 
+    Student student;
 
+@Before
+public void setup(){
+    student = new Student("Stephen","Pettus","cs9stephen@gmail.com","123");
+}
+
+@Test
+public void testGetFIrstName(){
+
+    String expected = "Stephen";
+    String acutal = student.getFirstName();
+
+    Assert.assertEquals(expected,acutal);
+
+}
 
 }
