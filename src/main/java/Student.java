a8c2346ce4ffaf933940c80f97c718d20464cf57
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-06-28 13:02:47 -0400

    init

---
+++ src/main/java/Student.java
@@ -0,0 +1,27 @@
+/*
+* A student must have these following instance variables
+* firstname: String
+* lastname: String
+* studentID: Int
+* email: String
+* password: String
+*
+* What can a student do?
+*
+* get and set all variables
+* e.g
+*
+* public void setFirstName(String newfirstname){
+* this.firstname = newfirstname;
+*
+* }
+*
+* public String getFirstName(){
+* return this.firstname;
+* }
+* */
+
+
+public class Student {
+
+}
