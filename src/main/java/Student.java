e2736d16e595aca187d2e0b8cd7a4e56af6dc82b
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-07-10 09:27:20 -0400

    Student work

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 public class Student extends Person {
 
     private int workedHours;
     private double money;
+    private double credits;
+
+
+
 
     private final double wage = 10.00;
 
@@ -11,15 +17,85 @@ public class Student extends Person {
 
         this.workedHours = 0;
         this.money = 0.00;
+        this.credits = 0;
     }
 
+
+
+    /*
+     * Getter method for the private variable workedHours
+     * return workedHours
+     * **/
+    public int getWorkedHours() {
+
+        return 0;
+    }
+
+    /*
+     * Setter method for worked hours
+     * set worked hours to be workedHours passed in
+     * **/
+    public void setWorkedHours(int workedHours) {
+
+    }
+
+    /*
+     * Getter method for private variable money
+     * return money
+     * **/
+    public double getMoney() {
+        return 0;
+    }
+    /*
+     * Setter for money
+     * **/
+    public void setMoney(double money) {
+
+    }
+
+    /*
+     * Getter method for private variable credits
+     * return credits
+     * **/
+    public double getCredits() {
+        return 0;
+    }
+
+    /*
+     * Setter method for private variable credits
+     * set credits
+     * **/
+    public void setCredits(double credits) {
+
+    }
+
+    /*
+    * The work method should take the param workedHours. Multiply it by the double wage and add the result to the current amount of money
+    * @param workedHours amount of hours a student can work
+    * **/
     public void work(int workedHours){
 
-        this.money += (workedHours * wage);
+
+
+    }
+    /*
+    * The learn method will be used to keep track of how many credits a student has. After they reach 123credits a student should graduate.
+    * every 10 hrs is 1 credit.
+    * classhours should be divided by 10 result should be added to credits
+    *
+    * **/
+    public void learn(int classHours){
 
     }
 
-    public void learn(int classHours){
+
+
+
+    public static void main(String args[]){
+
+        Teacher teacher = new Teacher("Stephen","Pettus","cs9stephen@gmail.com","123");
+        Student student = new Student("jamil","i","jamil@i.com","123");
+
 
     }
 
---
+++ src/main/java/Teacher.java
@@ -0,0 +1,5 @@
+public class Teacher extends Person {
+    public Teacher(String newFirstName, String newLastName, String newEmail, String newPassword) {
+        super(newFirstName, newLastName, newEmail, newPassword);
+    }
+}
