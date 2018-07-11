0ae577518369c2dc11ed6262777e819b57bb7056
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-07-11 13:51:42 -0400

    classroom

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -6,11 +6,21 @@ public class Teacher extends Person {
 
 
     /*
-    * This method will allow a teacher tp teach a student
+    * This method will allow a teacher to teach a student
     * how can this be done?
     * **/
     public void teach(Student student,int hoursTaught){
 
         student.learn(hoursTaught);
+    }
+
+    /*
+    This method will allow a teacher to teach multiple students
+    * how can this be done?
+    * hint you need a forloop
+    * **/
+    public void teachMultipleStudents(Student[] students, int hoursTaught){
+
+
     }
 }
