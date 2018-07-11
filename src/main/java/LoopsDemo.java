f6a8c0af7708585133c85eed68d279d3b5ddcd16
Ibrahim Branham <ibrahimbranham@Ibrahims-iMac.local>
2018-07-11 14:29:23 -0400

    sass-o-frass

---
+++ src/main/java/LoopsDemo.java
@@ -0,0 +1,32 @@
+public class LoopsDemo {
+
+    public static void main(String args[]){
+
+        int a = 0;
+
+        while (a < 100)  {
+            System.out.println(a);
+            a++;
+        }
+
+
+
+
+
+        for (int b = 0; b < 100; b++) {
+            System.out.println("hello from forloop aka for-loop");
+
+
+        }
+
+
+        System.out.println("Loop Phinished.");
+
+        a=0;
+        do{
+            System.out.println("Hello, World.");
+            a++;
+        }while (a < 10);
+
+        }
+}
