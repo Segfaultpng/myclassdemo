aced71fad8a6acf78dec45d72d7a56354ac6a6b4
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-07-13 09:09:31 -0400

    updating

---
+++ src/main/java/StringArrayUtils.java
@@ -0,0 +1,43 @@
+public class StringArrayUtils {
+
+    private String[] coolstrings;
+
+    public StringArrayUtils(String[] strings){
+
+        this.coolstrings = strings;
+    }
+
+    /*
+    * This method should loop through the coolstrings array with a for loop find the string passed
+    * in as a parameter with an if statement
+    *
+    * if you find the string return it
+    * if you don't return null
+    * **/
+    public String findString(String tofind){
+
+        return null;
+    }
+
+
+    /*
+    * This method should print all the strings found in coolstrings
+    * e.g
+    * if the array looks like this ["jamil","does","not","like","commitment"]
+    *
+    * then you should sout
+    * jamil
+    * does
+    * not
+    * like
+    * commitment
+    *
+    * use the new line character and a forloop to accomplish this
+    *
+    * hint to get the each string you can use coolstrings[some variable tied to the loop]
+    * **/
+    public String printStrings(String tofind){
+
+        return null;
+    }
+}
