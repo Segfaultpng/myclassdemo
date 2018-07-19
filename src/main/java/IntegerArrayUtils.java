4ed35c1e7edb8336f94292482de25af885831368
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-07-19 09:31:21 -0400

    IntArray

---
+++ src/main/java/IntegerArrayUtils.java
@@ -0,0 +1,42 @@
+public class IntegerArrayUtils {
+    private Integer[] coolInts;
+
+    public IntegerArrayUtils(Integer[] theints){
+
+        this.coolInts = theints;
+    }
+
+    /*
+     * This method should loop through the coolInts array with a for loop find the int passed
+     * in as a parameter with an if statement
+     *
+     * if you find the int return it
+     * if you don't return -1
+     * **/
+    public Integer findInt(Integer tofind){
+
+        return null;
+    }
+
+
+    /*
+     * This method should print all the int found in coolInts
+     * e.g
+     * if the array looks like this [0,1,2,3,4]
+     *
+     * then you should sout
+     * 0
+     * 1
+     * 2
+     * 3
+     * 4
+     *
+     * use the new line character and a forloop to accomplish this \n
+     *
+     * hint to get the each string you can use coolInts[some variable tied to the loop]
+     * **/
+    public String printInt(){
+
+        return null;
+    }
+}
